188e361a431da8dca1fdeaf1886e8d1bccea121a
lukas-swallville <lukas@fullstacklabs.co>
2023-01-02 15:41:44 -0300

    fix: lint fixes

--- monster/nested_serializers.py
+++ monster/nested_serializers.py
@@ -3,27 +3,12 @@ from rest_framework import serializers
 
 from battle.models import Battle
 from battle.nested_serializers import BattleListPKSerializer
+
+from monster.base_serializer import MonsterBaseSerializer
 from monster.models import Monster
 
-from utils.utils import CustomCharField
 
-
-class MonsterListRetrieveUpdateSerializer(serializers.ModelSerializer):
-    name = CustomCharField(
-        max_length=100,
-        allow_blank=False,
-    )
-
-    speed = serializers.IntegerField(min_value=0, max_value=100)
-
-    attack = serializers.IntegerField(min_value=0, max_value=100)
-
-    defense = serializers.IntegerField(min_value=0, max_value=100)
-
-    hp = serializers.IntegerField(min_value=0, max_value=100)
-
-    imageUrl = serializers.URLField()
-
+class MonsterListRetrieveUpdateSerializer(MonsterBaseSerializer):
     battles = serializers.SerializerMethodField("get_monster_battles")
 
     def get_monster_battles(self, obj):
--- monster/serializers.py
+++ monster/serializers.py
@@ -1,26 +1,10 @@
 from rest_framework import serializers
 
+from monster.base_serializer import MonsterBaseSerializer
 from monster.models import Monster
 
-from utils.utils import CustomCharField
 
-
-class MonsterSerializer(serializers.ModelSerializer):
-    name = CustomCharField(
-        max_length=100,
-        allow_blank=False,
-    )
-
-    speed = serializers.IntegerField(min_value=0, max_value=100)
-
-    attack = serializers.IntegerField(min_value=0, max_value=100)
-
-    defense = serializers.IntegerField(min_value=0, max_value=100)
-
-    hp = serializers.IntegerField(min_value=0, max_value=100)
-
-    imageUrl = serializers.URLField()
-
+class MonsterSerializer(MonsterBaseSerializer):
     class Meta:
         model = Monster
         fields = "__all__"
